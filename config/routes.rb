b8b5911b1c5999bb5cc1c7db90a362452ef0b4de
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-12-18 07:21:22 -0500

    add routes for genres and videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+
   get '/auth/:provider/callback' , to: 'sessions#create'
+
+  resources :genres do
+    resources :videos, only: [:index, :new, :create]
+  end
   
-  get 'sessions/new'
-  root 'application#home'
   resources :users, only: [:new, :create]
 
   get 'login', to: 'sessions#new'
   post 'sessions', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+
+  resources :videos, only: [:show, :edit, :update, :destroy]
+
+  root 'application#home'
 
 end
