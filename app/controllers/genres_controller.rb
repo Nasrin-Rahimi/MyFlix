0127fb41dd8d5664c3a426a2937635a8495e4b5a
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-12-19 08:12:56 -0500

    change basic authentication for creating genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,6 @@
 class GenresController < ApplicationController
 
-    http_basic_authenticate_with name: "dhhh", password: "secret", except: [:index, :show]
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
     
     def index
         @genres = Genre.all
