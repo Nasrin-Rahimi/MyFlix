3b1e009c6b37845806d78c9576955c7ce8b1f853
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-12-21 10:22:25 -0500

    Create a new review for a video

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,18 @@ class ReviewsController < ApplicationController
     end
 
     def create
+        @review = Review.new(video_id: params[:review][:video_id], user_id: current_user.id, rating: params[:review][:rating], description: params[:review][:description])
+        if @review.save
+            redirect_to video_path(@review.video)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:rating, :user_id, :video_id, :description)
     end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,12 +2,14 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :video
 
+  validates :rating, inclusion: { in: 1..5, message: " should be a number between 1 to 5" }
+
   def video_name
-    self.video.title
+    self.video.title if self.present? && self.video.present?
   end
 
-  def user_name
-    self.user.name
-  end
-  
+  # def user_name
+  #   self.user.name
+  # end
+
 end
