f8034b1eafa934b0285b3172e8ab2bd123151b90
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-12-21 14:20:36 -0500

    add authentication for user crud actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+  before_action :authentication_required, except: [:new, :create]
   before_action :set_user, only: [:show, :edit, :update]
 
   def show
