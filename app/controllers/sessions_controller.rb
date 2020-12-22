25e8de822377fb2998dc1ac53736c68221448b87
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-12-22 06:53:49 -0500

    handle error messages in login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,28 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     if auth_hash = request.env["omniauth.auth"]
       #The person 100% trusted coming from github
-      user  = User.find_or_create_by_omniauth(auth_hash)
-      login(user)
+      @user  = User.find_or_create_by_omniauth(auth_hash)
+      login(@user)
       redirect_to root_path
     else
       #Normal login with an email and password
-      user = User.find_by(name: params[:name])
-      if user && user.authenticate(params[:password])
-        login(user)
-        redirect_to root_path
+      if params[:name] == "" || params[:password] == ""
+        flash.now[:notice] ="Please enter User Name and Password"
+        render :new
       else
-        render 'sessions/new'
+        @user = User.find_by(name: params[:name])
+        if @user && @user.authenticate(params[:password])
+          login(@user)
+          redirect_to root_path
+        else
+          flash.now[:notice] ="User Name or Password is incorrect!"
+          render :new
+        end
       end
     end
   end
