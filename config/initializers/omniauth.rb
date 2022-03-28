a67198922fc2d515c460a7898d63b0ec873bae32
Nasrin-Rahimi <77.nasrin@gmail.com>
2022-03-28 07:22:21 -0400

    error debuging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     # provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+    #We're telling our Rails app to use a piece of middleware created by OmniAuth for the GitHub authentication strategy.
     provider :github, 'e34d8260405f64d20dc1', 'cefe52df15831f2eb8e8a12346c633bc4885847d'
 end
